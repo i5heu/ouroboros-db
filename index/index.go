e070d515f5b5519e1d913e5f30e4788b0de2b6a1
i5heu <i5heu@i5h.eu>
2025-11-18 17:11:04 +0000

    feat: add initial index package with TODOs for thread sorting and semantic text search implementation

---
+++ index/index.go
@@ -0,0 +1,5 @@
+package index
+
+//TODO : implement thread root to last child edit timestamp to optimize sorting by last activity
+
+//TODO: implement semantic text search with bleve vector search and nvidia/llama-embed-nemotron-8b embeddings
