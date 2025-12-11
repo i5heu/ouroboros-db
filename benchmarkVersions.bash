e8b3c0ab640fabd923500ed9506fd0c8accef583
i5heu <i5heu@i5h.eu>
2025-12-11 19:49:38 +0000

    fix: update benchmark script parameters for increased count and reduced time

--- benchmarkVersions.bash
+++ benchmarkVersions.bash
@@ -8,8 +8,8 @@ docker run --rm \
   -v "$PWD/.bench-html":/results-html \
   -e NUM_VERSIONS=3 \
   -e BENCH_PKG_PATTERN=./... \
-  -e BENCH_COUNT=10 \
-  -e BENCH_TIME=5s \
+  -e BENCH_COUNT=30 \
+  -e BENCH_TIME=2s \
   -e VERBOSE=1 \
   my-go-bench
 
