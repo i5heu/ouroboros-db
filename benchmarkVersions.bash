d4774bc9de31ed4f0903366d04a9833bed114c12
i5heu <i5heu@i5h.eu>
2025-12-11 20:23:00 +0000

    Refactor code structure for improved readability and maintainability

--- benchmarkVersions.bash
+++ benchmarkVersions.bash
@@ -5,7 +5,7 @@ docker build -f benchmark/Dockerfile -t my-go-bench .
 docker run --rm \
   -v "$PWD":/repo-src:ro \
   -v "$PWD/benchmark":/benchmark \
-  -v "$PWD/.bench-html":/results-html \
+  -v "$PWD/docs/bench-html":/results-html \
   -e NUM_VERSIONS=3 \
   -e BENCH_PKG_PATTERN=./... \
   -e BENCH_COUNT=30 \
