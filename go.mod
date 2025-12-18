09f3aa96b87fa7a79239a9437db23aceed76d331
i5heu <i5heu@i5h.eu>
2025-12-18 12:57:12 +0000

    feat: add LocalSealedSliceStore interface and Store implementation using BadgerDB

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/i5heu/ouroboros-db
 go 1.24.6
 
 require (
+	github.com/dgraph-io/badger/v4 v4.9.0
 	github.com/i5heu/ouroboros-crypt v1.1.1
 	github.com/klauspost/compress v1.18.0
 	github.com/klauspost/reedsolomon v1.12.6
@@ -10,12 +11,24 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/flatbuffers v25.2.10+incompatible // indirect
 	github.com/ipfs/go-log/v2 v2.9.0 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/whyrusleeping/chunker v0.0.0-20181014151217-fe64bd25879f // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
 
 require (
---
+++ pkg/localSealedSliceStore/store.go
@@ -0,0 +1,83 @@
+package localSealedSliceStore
+
+import (
+	"github.com/dgraph-io/badger/v4"
+	"github.com/i5heu/ouroboros-crypt/pkg/hash"
+	"github.com/i5heu/ouroboros-db/pkg/cas"
+)
+
+// LocalSealedSliceStore manages the local persistence of SealedSlices.
+// It uses BadgerDB as the underlying storage engine.
+type LocalSealedSliceStore interface {
+	// Store persists a SealedSlice associated with a public key hash.
+	// Returns the hash of the stored slice or an error.
+	Store(sealedSlice cas.SealedSlice, hashOfPubKey hash.Hash) (hash.Hash, error)
+
+	// Get retrieves a SealedSlice by its hash.
+	Get(sliceHash hash.Hash) (cas.SealedSlice, error)
+
+	// Delete removes a SealedSlice by its hash.
+	Delete(sliceHash hash.Hash) error
+
+	// ChunkListSealedSlices returns all SealedSlice hashes for a given Chunk
+	// hash.
+	ChunkListSealedSlices(chunkHash hash.Hash) ([]hash.Hash, error)
+
+	// ChunkListSealedSlicesForPubKey returns SealedSlice hashes for a given Chunk
+	// hash and Public Key hash.
+	ChunkListSealedSlicesForPubKey(
+		chunkHash, hashOfPubKey hash.Hash,
+	) ([]hash.Hash, error)
+}
+
+// Store implements LocalSealedSliceStore using BadgerDB.
+type Store struct {
+	db *badger.DB
+}
+
+// New creates a new Store instance.
+func New(db *badger.DB) *Store { // A
+	return &Store{
+		db: db,
+	}
+}
+
+// Store persists a SealedSlice associated with a public key hash.
+// Key format: [ChunkHash]:[HashOfPubkey]:[SealedSliceHash] (binary)
+func (s *Store) Store(
+	sealedSlice cas.SealedSlice,
+	hashOfPubKey hash.Hash,
+) (hash.Hash, error) { // A
+	// TODO: Implement storage logic
+	var zeroHash hash.Hash
+	return zeroHash, nil
+}
+
+// Get retrieves a SealedSlice by its hash.
+func (s *Store) Get(sliceHash hash.Hash) (cas.SealedSlice, error) { // A
+	// TODO: Implement retrieval logic
+	return cas.SealedSlice{}, nil
+}
+
+// Delete removes a SealedSlice by its hash.
+func (s *Store) Delete(sliceHash hash.Hash) error { // A
+	// TODO: Implement deletion logic
+	return nil
+}
+
+// ChunkListSealedSlices returns all SealedSlice hashes for a given Chunk hash.
+func (s *Store) ChunkListSealedSlices(
+	chunkHash hash.Hash,
+) ([]hash.Hash, error) { // A
+	// TODO: Implement listing logic
+	return nil, nil
+}
+
+// ChunkListSealedSlicesForPubKey returns SealedSlice hashes for a given Chunk
+// hash and Public Key hash.
+func (s *Store) ChunkListSealedSlicesForPubKey(
+	chunkHash, hashOfPubKey hash.Hash,
+) ([]hash.Hash, error) { // A
+	// TODO: Implement listing logic
+	return nil, nil
+}
